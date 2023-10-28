43a98d86ab6d84d98d188a72cd00903ac8781041
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2023-10-28 22:09:54 +0530

    user-mingle: setup scripts

---
+++ user-mingle/dags/kafka_stream.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner':'vedanth',
+    'start_date': datetime(2023,10,28,00)
+}
+
+def get_data():
+    import json
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+def stream_data():
+    import json
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res,indent = 3))
+    
+
+with DAG('user_automation',
+         default_args=default_args,
+         scheduer_interval = '@daily',
+         catchup = False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id = 'stream_data_from_api',
+        python_callable = stream_data
+    )
+
+stream_data()
