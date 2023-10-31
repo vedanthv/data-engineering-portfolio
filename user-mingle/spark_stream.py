c8e2db1bf51a2da93130409d9219cf75f4d21834
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2023-10-31 11:00:01 +0530

    user-mingle : spark connection code

---
+++ user-mingle/spark_stream.py
@@ -0,0 +1,26 @@
+import logging
+
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+
+def create_spark_connection():
+    s_conn = None
+
+    try:
+        s_conn = SparkSession.builder \
+            .appName('SparkDataStreaming') \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.cassandra.connection.host', 'localhost') \
+            .getOrCreate()
+
+        s_conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Spark connection created successfully!")
+    except Exception as e:
+        logging.error(f"Couldn't create the spark session due to exception {e}")
+
+    return s_conn
+
+
