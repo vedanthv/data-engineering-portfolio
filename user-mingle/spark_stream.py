dc922bcb9eafded24e15425745780349d70bbb1f
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2023-11-01 10:11:03 +0530

    user-mingle: cassandra connection

--- user-mingle/spark_stream.py
+++ user-mingle/spark_stream.py
@@ -23,4 +23,18 @@ def create_spark_connection():
 
     return s_conn
 
+def create_cassandra_connection():
+    try:
+        # connecting to the cassandra cluster
+        cluster = Cluster(['localhost'])
+
+        cas_session = cluster.connect()
+
+        return cas_session
+    except Exception as e:
+        logging.error(f"Could not create cassandra connection due to {e}")
+        return None
+
+
+
 
