158e5e62ef6cd5de7f8f4bae7af822d05b07a72d
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2023-11-02 10:47:17 +0530

    user-mingle: fn to create and insert data into the table

--- user-mingle/spark_stream.py
+++ user-mingle/spark_stream.py
@@ -32,6 +32,52 @@ def create_keyspace():
 
     print("Keyspace created successfully!")
 
+def create_table(session):
+    session.execute("""
+    CREATE TABLE IF NOT EXISTS spark_streams.created_users (
+        id UUID PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        address TEXT,
+        post_code TEXT,
+        email TEXT,
+        username TEXT,
+        registered_date TEXT,
+        phone TEXT,
+        picture TEXT);
+    """)
+
+    print("Table created successfully!")
+
+
+def insert_data(session, **kwargs):
+    print("inserting data...")
+
+    user_id = kwargs.get('id')
+    first_name = kwargs.get('first_name')
+    last_name = kwargs.get('last_name')
+    gender = kwargs.get('gender')
+    address = kwargs.get('address')
+    postcode = kwargs.get('post_code')
+    email = kwargs.get('email')
+    username = kwargs.get('username')
+    dob = kwargs.get('dob')
+    registered_date = kwargs.get('registered_date')
+    phone = kwargs.get('phone')
+    picture = kwargs.get('picture')
+
+    try:
+        session.execute("""
+            INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
+                post_code, email, username, dob, registered_date, phone, picture)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (user_id, first_name, last_name, gender, address,
+              postcode, email, username, dob, registered_date, phone, picture))
+        logging.info(f"Data inserted for {first_name} {last_name}")
+
+    except Exception as e:
+        logging.error(f'could not insert data due to {e}')
 
 
 def create_cassandra_connection():
